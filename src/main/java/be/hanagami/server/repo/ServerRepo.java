781b36e012603ea9a74ec9d46897834f72a9894d
Hanagami3 <hanaga3mi@gmail.com>
2024-06-17 01:01:23 +0200

    first commit

---
+++ src/main/java/be/hanagami/server/repo/ServerRepo.java
@@ -0,0 +1,8 @@
+package be.hanagami.server.repo;
+
+import be.hanagami.server.model.Server;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServerRepo extends JpaRepository<Server, Long> {
+    Server findByIpAddress(String ipAddress);
+}
